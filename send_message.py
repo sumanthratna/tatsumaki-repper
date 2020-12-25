ea00ad9dc2892e68bd2966fe9987137f2c9f8233
Sumanth Ratna <2022sratna@tjhsst.edu>
2020-12-25 12:21:35 -0500

    Fix script

--- send_message.py
+++ send_message.py
@@ -4,8 +4,8 @@ import asyncio
 
 
 client = discord.Client()
-DISCORD_CHANNEL_ID = getenv('DISCORD_CHANNEL_ID')
-DISCORD_USER_ID = getenv('DISCORD_USER_ID')
+DISCORD_CHANNEL_ID = int(getenv('DISCORD_CHANNEL_ID'))
+DISCORD_USER_ID = int(getenv('DISCORD_USER_ID'))
 DISCORD_AUTH_TOKEN = getenv('DISCORD_AUTH_TOKEN')
 
 
@@ -15,8 +15,8 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    channel = discord.get_channel(DISCORD_CHANNEL_ID)
-    await client.send_message(channel, f't!rep <@{DISCORD_USER_ID}>')
-    client.close()
+    channel = client.get_channel(DISCORD_CHANNEL_ID)
+    await channel.send(f't!rep <@{DISCORD_USER_ID}>')
+    await client.close()
 
 client.run(DISCORD_AUTH_TOKEN, bot=False)
