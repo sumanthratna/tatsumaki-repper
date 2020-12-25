5f6fa697cc3cf61d58d6f983665658d690d6cdfc
Sumanth Ratna <2022sratna@tjhsst.edu>
2020-12-25 12:03:00 -0500

    Remove event loop from Python

---
+++ send_message.py
@@ -0,0 +1,21 @@
+from os import getenv
+import discord
+import asyncio
+
+
+client = discord.Client()
+DISCORD_CHANNEL_ID = getenv('DISCORD_CHANNEL_ID')
+DISCORD_USER_ID = getenv('DISCORD_USER_ID')
+DISCORD_AUTH_TOKEN = getenv('DISCORD_AUTH_TOKEN')
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    channel = discord.get_channel(DISCORD_CHANNEL_ID)
+    await client.send_message(channel, f't!rep <@{DISCORD_USER_ID}>')
+    client.close()
+
+client.run(DISCORD_AUTH_TOKEN, bot=False)
